838f1805bf5984532fff76b7af37958fc5f37961
Tobias <tobis-stroedicke@gmx.de>
2019-01-16 13:51:27 +0100

    Unity Window to load level

---
+++ LevelEditor/Unity/LoadLevel/Assets/Editor/LevelEditor/LevelEditorWindow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using LevelFramework;
+using LevelEditor;
+
+public class LevelEditorWindow : EditorWindow
+{
+
+    [MenuItem("Window/LevelEditor")]
+    public static void ShowWindow()
+    {
+        GetWindow(typeof(LevelEditorWindow));
+    }
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("Open File"))
+        {
+            LevelManager lvlManager = new LevelManager();
+            Level loadedLevel = lvlManager.LoadLevel(EditorUtility.OpenFilePanel("Level to be loaded...", "", "lvl"));
+        }
+    }
+}
